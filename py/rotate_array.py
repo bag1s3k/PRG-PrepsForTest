0d2329f6c69f9c0c58603775cfc5fffa7421a2ff
bag1s3k <jetam.mravenec@gmail.com>
2025-11-24 19:04:57 +0100

    Rotate array

---
+++ py/rotate_array.py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def rotate(self, nums: list[int], k: int) -> list[int]:
+        k %= len(nums)
+        return [*nums[-k:], *nums[:-k]]
+
+
+solution = Solution()
+print([*solution.rotate([1,2,3,4,5,6,7], 3)])
+print([*solution.rotate([-1,-100,3,99], 2)])
