2b9bd977d6648d5ee15218d634063ce4a6112712
bag1s3k <jetam.mravenec@gmail.com>
2025-11-24 19:05:12 +0100

    Pow

---
+++ py/pow.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x ** n
+
+solution = Solution()
+print(solution.myPow(2.00000, 10))
+print(solution.myPow(2.10000, 3))
+print(solution.myPow(2.0, -2))
