16eccd846940e677d46513967536e30580adde73
bag1s3k <jetam.mravenec@gmail.com>
2025-11-25 12:57:50 +0100

    Group anagrams

---
+++ py/group_anagrams.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        tmp = {}
+        for string in strs:
+            key = "".join(sorted(string))
+            if key not in tmp:
+                tmp[key] = []
+            tmp[key].append(string)
+
+        return list(tmp.values())
+
+solution = Solution()
+print(solution.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(solution.groupAnagrams([""]))
+print(solution.groupAnagrams(["a"]))
