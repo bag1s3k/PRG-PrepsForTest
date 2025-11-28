69548c6c5c823ff697936b421a2492b8ce196bf7
bag1s3k <jetam.mravenec@gmail.com>
2025-11-28 10:49:23 +0100

    subsets

---
+++ py/subsets.py
@@ -0,0 +1,19 @@
+﻿class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        result = []
+        nums.sort()
+        def backtrack(start, current):
+            result.append(list(current))
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                current.append(nums[i])
+                backtrack(i+1, current)
+                current.pop()
+        backtrack(0, [])
+        return result
+
+
+solution = Solution()
+print(solution.subsets([1,2,3]))
+print(solution.subsets([0]))
