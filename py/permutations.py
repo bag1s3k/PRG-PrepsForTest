7ef7c40b06e8426508e5d61a736d5d601ae42194
bag1s3k <jetam.mravenec@gmail.com>
2025-11-28 10:25:09 +0100

    Permutations

---
+++ py/permutations.py
@@ -0,0 +1,12 @@
+﻿from itertools import permutations
+from math import factorial
+
+class Solution:
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        return [list(item) for item in permutations(nums)]
+
+solution = Solution()
+print(solution.permute([1,2,3]))
+print(solution.permute([0,1]))
+print(solution.permute([1]))
+print(solution.permute([5,4,6,2]))
